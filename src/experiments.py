222c6f6f92cb433f488b725528dcaad60f29a373
GalMoore <gal@galmoore.co.uk>
2019-08-08 20:49:47 +0300

    path

--- src/experiments.py
+++ src/experiments.py
@@ -1,8 +1,11 @@
 import subprocess
 import os
 
+from os.path import expanduser
+home = expanduser("~") + "/"
+# os.system("python3 {}catkin_ws/src/robot_face/src/headturn.py {}".format(home,str(5)))
 
 # python_bin3 = "/usr/bin/python3"
 # subprocess.Popen([python_bin3, "/home/gal/catkin_ws/src/robot_face/src/go_left.py"]).wait()
 
-os.system("python3 /home/intel/catkin_ws/src/robot_face/src/go_left.py 5")
+os.system("python3 {}catkin_ws/src/robot_face/src/go_left.py 5".format(home))
